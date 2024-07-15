7ad96cad9dc55845a87e0fdb4b3145021409c173
OboltusAli <107478063+OboltusAli@users.noreply.github.com>
2024-07-15 11:29:23 +0300

    Update admin.js

--- src/lib/supabase/admin.js
+++ src/lib/supabase/admin.js
@@ -1,3 +1,4 @@
+import { createClient } from '@supabase/supabase-js'
 import { PUBLIC_SUPABASE_URL } from '$env/static/public';
 import { PRIVATE_SUPABASE_PRIVATE_KEY } from '$env/static/private';
 
