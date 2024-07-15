a13f321c99413f46d6ab619304a601daf4bacbeb
OboltusAli <107478063+OboltusAli@users.noreply.github.com>
2024-07-15 11:26:16 +0300

    Update admin.js

--- src/lib/supabase/admin.js
+++ src/lib/supabase/admin.js
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js'
+import { PUBLIC_SUPABASE_URL } from '$env/static/public';
+import { PRIVATE_SUPABASE_PRIVATE_KEY } from '$env/static/private';
 
-
-export default createClient("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impzcnhsa29ienNtdmZ5cGNlcWpzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjEwMjk3NjMsImV4cCI6MjAzNjYwNTc2M30.JJeCCFcQEEOpBqyCsbTQFO4dU_ZrUe1_nuE2jIV-BXI", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impzcnhsa29ienNtdmZ5cGNlcWpzIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyMTAyOTc2MywiZXhwIjoyMDM2NjA1NzYzfQ.51KIssSG6P_o0NG8cLi0ajEe_pOTA33hfhiBtP7XFe0")
+export default createClient(PUBLIC_SUPABASE_URL, PRIVATE_SUPABASE_PRIVATE_KEY)
